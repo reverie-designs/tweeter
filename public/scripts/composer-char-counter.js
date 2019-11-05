58f24df97df1edf621e6c8150842f0e0872f795d
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-05 20:50:07 +0000

    improved on char counter function by optimizing document search for elements into variables

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,24 @@
 //jQuery
 
 $(function() {
-  //subtracts text area value from 140 on key up
-  $(".new-tweet > form > textarea").on('keyup', () => {
+
+  let $tweet = ".new-tweet > form > textarea";
+  let $tweetCounter = $tweet + "~" + ".counter";
+
+  $($tweet).on('keyup', () => {
 
     let $textArea = $(".new-tweet > form > textarea").val();
     let $textLength = 140 - $textArea.length;
     
-    //turn counter red if it hits below zero by adding a class to it.
-    $(".counter").html($textLength);
+    //update character counter html
+    $($tweetCounter).html($textLength);
+
+    //turn counter red if it hits below zero by adding a class to it
     if($textLength < 0){
-      $(".counter").addClass("turnRed");
+      $($tweetCounter).addClass("turnRed");
     }
     if($textLength > 0) {
-      $(".counter").removeClass("turnRed");
+      $($tweetCounter).removeClass("turnRed");
     }
   });
 
