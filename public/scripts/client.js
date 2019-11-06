634d0f385ccdb5499193c3ad84ca1185e735a709
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-06 21:46:11 +0000

    substituted hard coded tweet object with ajax get and post requests for the tweet database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,5 @@
 //client.js
 
-//sample tweet database
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
 //function that converts input data to text protecting from script injection
 const escape = function(str) {
   const div = document.createElement('div');
@@ -37,7 +11,7 @@ const escape = function(str) {
 const getTimeOfTweet = (milsecs) => {
   let result = [];
   const today = new Date();
-  let timeAgo = (today - milsecs)/86400000;
+  const timeAgo = (today - milsecs)/86400000;
   
   result.push(Math.ceil(timeAgo)); 
 
@@ -45,45 +19,61 @@ const getTimeOfTweet = (milsecs) => {
   if (toString(result[0]).length > 1 || result[0] !== 1) {
     result.push('s');
   }
-
   return result;
 };
 
+// Creates Tweet Article Element with the data rendered from Json Object
+const createTweetElement = (tweetObject)=> {
+  let timeAgo = getTimeOfTweet(tweetObject.created_at);
+  const $tweet = `
+    <article class="tweet">
+      <header>
+          <img class="user-image" src="${escape(tweetObject.user.avatars)}" alt="profile image of ${escape(tweetObject.user.name)}">
+          <p>
+            <span class="user-name">${escape(tweetObject.user.name)}</span>
+            <span class="tweet-tag">${escape(tweetObject.user.handle)}</span>
+          </p>
+      </header>
+      <p class="tweet-message">${escape(tweetObject.content.text)}</p>
+      <footer>
+        <span>${timeAgo[0]} day${timeAgo[1]}</span>
+        <span class="tweet-icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-heart"></i>
+          <i class="fas fa-retweet"></i>
+        </span>
+      </footer>
+    </article>`;
+  return $tweet;
+};
+
+//for every tweet in tweetDatabase create an html element and append it to the body
+const renderTweets = (tweetDatabase) => {
+  for(let tweet of tweetDatabase){
+    $("#get-tweets").append(createTweetElement(tweet));
+  }
+};
+
+//==========================================================
 //jQuery rendering of tweets
 $(function(){
 
-  // Creates Tweet Article Element with the data rendered from Json Object
-  const createTweetElement = (tweetObject)=> {
-    let timeAgo = getTimeOfTweet(tweetObject.created_at);
-    const $tweet = `
-      <article class="tweet">
-        <header>
-            <img class="user-image" src="${escape(tweetObject.user.avatars)}" alt="profile image of ${escape(tweetObject.user.name)}">
-            <p>
-              <span class="user-name">${escape(tweetObject.user.name)}</span>
-              <span class="tweet-tag">${escape(tweetObject.user.handle)}</span>
-            </p>
-        </header>
-        <p class="tweet-message">${escape(tweetObject.content.text)}</p>
-        <footer>
-          <span>${timeAgo[0]} day${timeAgo[1]}</span>
-          <span class="tweet-icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-heart"></i>
-            <i class="fas fa-retweet"></i>
-          </span>
-        </footer>
-      </article>`;
-    return $tweet;
-  };
-
-  //for every tweet in tweetDatabase create an html element and append it to the body
-  const renderTweets = (tweetDatabase) => {
-    for(let tweet of tweetDatabase){
-      $("#get-tweets").append(createTweetElement(tweet));
-    }
-  };
-
-  renderTweets(data);
+  //ajax add tweet to database
+  $('#tweet-form').on('submit', function (event) {
+    event.preventDefault();
+    console.log($(this));
+    $.ajax({
+        url : '/tweets/',
+        type: 'POST',
+        data : $(this).serialize()
+    });
+  });
+  
+  //ajax get tweet from database
+  $.ajax('/tweets/', { method: 'GET' })
+    .then(function (getTweets) {
+      console.log('Success: ', getTweets);
+      renderTweets(getTweets);
+  });
   
 });
