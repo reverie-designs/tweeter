4f2d7d72a1a77c8ce757d7cda3d45eefd72ac92a
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-06 19:56:50 +0000

    adjusted function that returns days elapsed since post  and day or days depending on value

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,4 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+//client.js
 
 //sample tweet database
 const data = [
@@ -30,7 +26,7 @@ const data = [
   }
 ]
 
-//function that converts input data to text cleaning in case somepne inputs script
+//function that converts input data to text protecting from script injection
 const escape = function(str) {
   const div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
@@ -42,10 +38,14 @@ const getTimeOfTweet = (milsecs) => {
   let result = [];
   const today = new Date();
   let timeAgo = (today - milsecs)/86400000;
+  
   result.push(Math.ceil(timeAgo)); 
-  if(toString(result[0]).length > 1){
+
+  //adds an 's' if it has been more than a single day ago
+  if (toString(result[0]).length > 1 || result[0] !== 1) {
     result.push('s');
   }
+
   return result;
 };
 
@@ -55,22 +55,32 @@ $(function(){
   // Creates Tweet Article Element with the data rendered from Json Object
   const createTweetElement = (tweetObject)=> {
     let timeAgo = getTimeOfTweet(tweetObject.created_at);
-    const $tweet = `<article class="tweet">
-    <header>
-        <img class="user-image" src="${escape(tweetObject.user.avatars)}" alt="">
-        <p><span class="user-name">${escape(tweetObject.user.name)}</span>
-          <span class="tweet-tag">${escape(tweetObject.user.handle)}</span></p>
-      </header>
-      <p class="tweet-message">${escape(tweetObject.content.text)}</p>
-      <footer><span> ${timeAgo[0]} day${timeAgo[1]}</span><span class="tweet-icons"><i class="fas fa-flag"></i><i class="fas fa-heart"></i><i class="fas fa-retweet"></i></span></footer>
-    </article>`;
+    const $tweet = `
+      <article class="tweet">
+        <header>
+            <img class="user-image" src="${escape(tweetObject.user.avatars)}" alt="profile image of ${escape(tweetObject.user.name)}">
+            <p>
+              <span class="user-name">${escape(tweetObject.user.name)}</span>
+              <span class="tweet-tag">${escape(tweetObject.user.handle)}</span>
+            </p>
+        </header>
+        <p class="tweet-message">${escape(tweetObject.content.text)}</p>
+        <footer>
+          <span>${timeAgo[0]} day${timeAgo[1]}</span>
+          <span class="tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-heart"></i>
+            <i class="fas fa-retweet"></i>
+          </span>
+        </footer>
+      </article>`;
     return $tweet;
   };
 
   //for every tweet in tweetDatabase create an html element and append it to the body
   const renderTweets = (tweetDatabase) => {
     for(let tweet of tweetDatabase){
-      $("#newTweets").append(createTweetElement(tweet));
+      $("#get-tweets").append(createTweetElement(tweet));
     }
   };
 
