e04c3367164da988f20c0c58e57bba4a5079f2d4
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-07 21:58:34 +0000

    made the new tweet button toggle fade when the back to top button appears

--- public/scripts/top-button.js
+++ public/scripts/top-button.js
@@ -1,8 +1,10 @@
 $(window).scroll(function() {
   const height = $(window).scrollTop();
-  if (height > 100) {
+  if (height > 90) {
     $('.icon-top').fadeIn();
+    $('.tweet-toggle').fadeOut();
   } else {
+    $('.tweet-toggle').fadeIn();
     $('.icon-top').fadeOut();
   }
 });
